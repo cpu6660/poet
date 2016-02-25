19da41880acac8b9ae2fe27ee93163644daf1267
chenpeng <820238985@qq.com>
2016-02-25 13:19:01 +0800

    update

--- index.php
+++ index.php
@@ -8,55 +8,9 @@ use Poet\Framework\Util\Collection;
 use Poet\Framework\Util\Contract\Arrayable;
 use Illuminate\Support\Arr;
 
-$target = [
-    'a' => [
-        'd' => [
-            'q' => 'eee',
-            'u' => 'qqqqq'
-        ],
-        'e' => '456',
-        'f' => '789'
-    ],
-    'b' => [
-        'g' =>'111',
-        'h' =>'222',
-        'k' =>'333',
-        'f' => 'rrrr'
-    ],
-    'c' => [
-        'i'=>'555',
-        'j'=>'666',
-        'o'=>'999'
-    ]
-];
-
-$container  = new Container();
-
-class People {
-    public $name = "chen";
-    public function show(){
-        echo $this->name;
-    }
-}
-
-$container['people'] = function(){
-   return new People();
-};
-
-$mypeople = $container['people'];
-
-$mypeople->show();
-$mypeople->name = 'peng';
 
 
-$cc = $container['people'];
 
-$cc->name = "eeeee";
-
-$dd = $container['people'];
-
-
-$dd->show();
 
 
 
